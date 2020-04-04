c064aab6e6a7fd3c8df76fe7c3cebc8e3cb79ed1
Darshan Kathiriya <darshan@pop-os.localdomain>
2020-04-04 19:55:40 -0300

    Replace Chat with new

--- Milestone 3/feed_bot_app/src/components/Chat.js
+++ Milestone 3/feed_bot_app/src/components/Chat.js
@@ -1,88 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { SideMenu } from "./SideMenu";
-import TopBar from "./TopBar";
-import {
-  Header,
-  Divider,
-  Grid,
-  Container,
-  Form,
-  Button,
-} from "semantic-ui-react";
-import { ChatFeed, Message } from "react-chat-ui";
+import { Form, Button } from "semantic-ui-react";
+import { API_URL } from "../constants/urls";
+import { Widget, addResponseMessage, toggleWidget } from "react-chat-widget";
+
+import "react-chat-widget/lib/styles.css";
 
 function Chat() {
-  const [currentMsg, setcurrentMsg] = useState("");
-  const [messages, setMessages] = useState([
-    new Message({
-      id: 1,
-      message: "I'm the recipient! (The person you're talking to)",
-    }),
-    new Message({ id: 0, message: "I'm you -- the blue bubble!" }),
-  ]);
-
   useEffect(() => {
-    fetchMessages();
+    toggleWidget();
+    addResponseMessage("Hi! How are you doing?");
   }, []);
 
-  const fetchMessages = () => {};
+  const sendMessage = (msg) => {
+    fetch(API_URL + "/chat", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify({ answer: msg }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.code === 1) {
+          addResponseMessage(responseJson.next_question);
+        }
+      });
+  };
 
   return (
-    <Container fluid style={{ minHeight: "100vh" }}>
-      <TopBar />
-      <Grid columns={2} style={{ height: "100%" }}>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <SideMenu activeItem="forms" />
-          </Grid.Column>
-          <Grid.Column width={12} style={{ paddingTop: "3.5rem" }}>
-            <Header>Chat</Header>
-            <Divider />
-            <Grid>
-              <Grid.Column width={8}>
-                <ChatFeed
-                  messages={messages} // Boolean: list of message objects
-                  hasInputField={false} // Boolean: use our input, or use your own
-                  showSenderName // show the name of the user who sent the message
-                  bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
-                  // JSON: Custom bubble styles
-                  bubbleStyles={{
-                    text: {
-                      fontSize: 18,
-                    },
-                    chatbubble: {
-                      borderRadius: 70,
-                      padding: 8,
-                    },
-                  }}
-                />
-                <Grid.Row>
-                  <Form reply>
-                    <Form.TextArea
-                      value={currentMsg}
-                      onChange={(e, { value }) => {
-                        setcurrentMsg(value);
-                      }}
-                    />
-                    <Button
-                      content="Send"
-                      labelPosition="left"
-                      icon="send"
-                      primary
-                      onClick={() => {
-                        let msg = new Message({ id: 0, message: currentMsg });
-                        setMessages([...messages, msg]);
-                        setcurrentMsg("");
-                      }}
-                    />
-                  </Form>
-                </Grid.Row>
-              </Grid.Column>
-            </Grid>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ width: "320px", alignSelf: "center" }}>
+        <Widget
+          handleNewUserMessage={sendMessage}
+          title="My new awesome title"
+          subtitle="And my cool subtitle"
+        />
+      </div>
+    </div>
   );
 }
 
